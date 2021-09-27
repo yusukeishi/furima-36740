ebf01771085f1ad02f7c9df7efac90106902b6ae
yusukeishi <ishisida777@icloud.com>
2021-09-27 17:20:33 +0900

    商品出品情報のモデルとテーブル作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,18 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.all
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit()
+  end
+
 end

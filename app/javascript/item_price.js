ce25730701008938118b6847444fa8b05f325ae6
yusukeishi <ishisida777@icloud.com>
2021-10-01 15:10:11 +0900

    修正２

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,9 +5,11 @@ window.addEventListener('load', () => {
   
     const addTaxDom = document.getElementById("add-tax-price");
     const ProfitDom = document.getElementById("profit");
-    // 入力した金額をもとに販売手数料を計算する処理
+
+    // どこの処理かわかる為、販売手数料(10%)部分, 入力した金額をもとに販売手数料を計算する処理
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-    // 出品価格から販売手数料を引く処理
+
+    // どこの処理かわかる為, 販売利益部分, 出品価格から販売手数料を引く処理
     ProfitDom.innerHTML = inputValue - addTaxDom.innerHTML;
     })
 });

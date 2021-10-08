1583b5668d88637aabdf4116378e3b27a1c7d512
yusukeishi <ishisida777@icloud.com>
2021-10-08 17:15:41 +0900

    修正

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,7 @@ class Order
     validates :item_id
 
     validates :post_number, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
     validates :phone_number, format: { with: /\A[0-9]{11}\z/, message: 'is invalid' }
--- app/models/status.rb
+++ app/models/status.rb
@@ -1,7 +1,7 @@
 class Status < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: '商品・未使用' },
+    { id: 2, name: '新品・未使用' },
     { id: 3, name: '未使用に近い' },
     { id: 4, name: '目立った傷や汚れなし' },
     { id: 5, name: 'やや傷や汚れあり' },
